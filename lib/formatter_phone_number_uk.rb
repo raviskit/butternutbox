0a091b7a3cde20afdfdd36f9bad3f4b9dcf9e247
Ravi Prakash <raviskit2012@gmail.com>
2020-08-31 05:55:47 +0530

    Implemented format method with all the test cases passed

--- lib/formatter_phone_number_uk.rb
+++ lib/formatter_phone_number_uk.rb
@@ -2,7 +2,23 @@
 module Formatter
   module PhoneNumber
     module UK
+      NUMBER_VALIDATOR = /^(\+447\s?\d{9}|447\s?\d{9}|07\s?\d{9})?$/
+
+      # Method to format given number in +44 format.
+      # @param : number
+      # number should be provided as string for matching and formatting.
       def self.format(number)
+        number = number.strip.gsub(" ", "") # remove whitespaces
+        raise "Invalid number" if (NUMBER_VALIDATOR =~ number).nil?
+
+        leading_number = number[0]
+        if leading_number == "4"
+          number = number.prepend("+")
+        elsif leading_number == "0"
+          number = "+44" + number[1..number.length-1]
+        else
+          number
+        end
       end
     end
   end
--- spec/formatter_phone_number_uk_spec.rb
+++ spec/formatter_phone_number_uk_spec.rb
@@ -1,16 +1,57 @@
 require 'spec_helper'
+require 'formatter_phone_number_uk'
 
 describe "Formatter::PhoneNumber::UK" do
-  # TODO:
+
   describe "#format" do
     context "When number is invalid" do
-      it "Should raise error as invalid number"
+      it "Should raise error as invalid number when length of number exceeds" do
+        number = "071234567890"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+      end
+
+      it "Should raise error as invalid number when it doesn't start with +44 or 44 or 0" do
+        number = "71234567890"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+      end
+
+      it "Should raise error as invalid number when number is contains invalid characters" do
+        number = "7123456abc90"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+      end
+
+      it "Should raise error as invalid number when it starts with +44 or 44 or 0 but is of invalid length" do
+        number = "071234"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+      end
+
+      it "Should raise error as invalid number when it starts with +44 or 44 or 0 but don't have 7 after the prefix" do
+        number = "08123456789"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+
+        number = "+448123456789"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+
+        number = "448123456789"
+        expect{ Formatter::PhoneNumber::UK.format(number)}.to raise_error("Invalid number")
+      end
     end
 
     context "When number is valid" do
-      it "when input number is in +44 format, output should be in +44 format"
-      it "when input number is in 44 format, output should be in +44 format"
-      it "when input number is in 07 format, output should be in +44 format"
+      it "when input number is in +44 format, output should be in +44 format" do
+        number = "+447123456789"
+        expect(Formatter::PhoneNumber::UK.format(number)).to eq("+447123456789")
+      end
+
+      it "when input number is in 44 format, output should be in +44 format" do
+        number = "447123456789"
+        expect(Formatter::PhoneNumber::UK.format(number)).to eq("+447123456789")
+      end
+
+      it "when input number is in 07 format, output should be in +44 format" do
+        number = "07123456789"
+        expect(Formatter::PhoneNumber::UK.format(number)).to eq("+447123456789")
+      end
     end
   end
 
